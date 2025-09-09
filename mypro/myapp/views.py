3d4da26ff823a2b16149ed84a0b65db76e593c63
Musty-string3 <shuya199309@gmail.com>
2025-09-09 18:52:51 +0900

    PostViewに誤ったコードを記述 ruffのコードチェックは易しめ2回目

--- mypro/myapp/views.py
+++ mypro/myapp/views.py
@@ -41,6 +41,7 @@ class PostsView(View):
                 return redirect("myapp:posts")
 
             print(title, text, immmg)
+            print(title, text, immmggggg)
             Post.objects.create(title=title, text=text, img=img)
             request.session["message"] = "記事の作成に成功しました。"
 
